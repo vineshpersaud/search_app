90d37c1a7de033a4718fa87b826126899334a86f
github username <github email address>
2019-07-08 12:48:11 -0400

    done live reload working

--- src/Cars.js
+++ src/Cars.js
@@ -1,11 +1,39 @@
 import React,{Component} from 'react';
 
 class Cars extends Component{
+  constructor(){
+    super()
+    this.state = {
+      cars : ['Toyota','Honda','Audi','Lexus','Nissan'],
+      carsearch : ''
+    }
+  }
+
+  handleChange = (event) =>{
+    this.setState({carsearch: event.target.value});
+  }
 
   render(){
+    let search = this.state.cars.filter( (car) => {
+      return car.toLowerCase().indexOf(this.state.carsearch.toLowerCase()) !== -1;
+      }
+    )
     return(
       <div>
         <h1>Cars</h1>
+        <label>
+          Search Cars:
+          <input type="text"
+                 value={this.state.carsearch}
+                 onChange={this.handleChange}
+          />
+        </label>
+        {search.map(car=>
+          <div>
+            <h4>{car}</h4>
+          </div>
+          )
+        }
       </div>
     )
   }
